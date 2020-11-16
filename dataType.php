ce295a86fe68067b819987baac2185ad010ef9b4
Fanhengshuo <73242077+Fanhengshuo@users.noreply.github.com>
2020-11-16 20:02:44 +0800

    Add files via upload

---
+++ dataType.php
@@ -0,0 +1,21 @@
+<?php
+//$val = true;
+//$val = false;
+//echo $val; 
+//$val = 10;
+//$val1 = 10.1;
+//$val = "abc";
+//$val1 = '123';
+//$val = [1,2,3];
+//$val1 = array(1,2,3);
+// . 表示拼接
+$val = "anc";
+echo $val."123";
+//类与对象
+class Person{
+
+}
+$p = new Person();
+var_dump($p);//测试
+
+?>
